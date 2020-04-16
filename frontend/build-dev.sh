2b1e0c4f9a652bc9c1c868d40c74f9a315e328b9
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-04-15 21:09:10 -0400

    6-71 Starting the Container

---
+++ frontend/build-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Specify docker file other than Dockerfile with -f option
+docker build -f Dockerfile.dev .
---
+++ frontend/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# docker run -it -p 3000:3000 <IMAGE_ID>
+docker run -it -p 3000:3000 3d9c877ab71c
