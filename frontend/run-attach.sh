0081ff236c9950005ca8d0394ee1e59acda6c1db
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-04-19 14:12:23 -0400

    6-83 Shortcomings on Testing

---
+++ frontend/run-attach.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 6-83: Shortcomings on Testing
+# Use frontend_web CONTAINER_ID
+# docker attach <CONTAINER_ID>
+docker attach 82fab050f34e
---
+++ frontend/run-exec.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 6-83: Shortcomings on Testing
+# Use frontend_web CONTAINER_ID
+# docker exec -it <CONTAINER_ID> sh
+docker exec -it 82fab050f34e sh
