7773d8d4be3dce4f3fd3f6502aa50d3a7e0b0712
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2020-05-19 15:24:15 -0400

    14-210 Recreating the Deployment

--- complex-elastic-beanstalk/client/README.md
+++ complex-elastic-beanstalk/client/README.md
@@ -89,7 +89,7 @@ $> docker run -it <IMAGE_ID>
 Map port to be able to access application at http://localhost:3000
 ```
 $> docker run -it -p 3000:3000 <IMAGE_ID>
-```
+
 ### Section 13 - Maintaining Sets of Containers with Deployments
 
 #### Lecture 203 - Imperatively Updating a Deployment's Image
---
+++ complex-elastic-beanstalk/client/src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render } from '@testing-library/react';
+
+test('renders without crashing', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
